185e5ae3042758d9007817fe3b7ceb6ecbf6ac64
kponi5188 <kponi5188@gmail.com>
2026-04-08 02:00:20 +0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'developers.cjdropshipping.com' },
+      { protocol: 'https', hostname: '**' },
+    ],
+  },
+};
+
+export default nextConfig;
